34ec9dbfbf72b06fbc18bedffd5fdcbd58be2b1b
Dandd6541 <Mordkid4@gmail.com>
2022-08-05 14:55:35 -0700

    bones of pt 1 finished

--- main_app/views.py
+++ main_app/views.py
@@ -1,11 +1,29 @@
 from django.shortcuts import render
-
-# Add the following import
 from django.http import HttpResponse
+
+
+class Finch:  
+  def __init__(self, name, breed, description, age):
+    self.name = name
+    self.breed = breed
+    self.description = description
+    self.age = age
+
+finches = [
+  Finch('Lolo', 'tabby', 'foul little demon', 3),
+  Finch('Sachi', 'tortoise shell', 'diluted tortoise shell', 0),
+  Finch('Raven', 'black tripod', '3 legged finch', 4)
+]
+
 
 # Define the home view
 def home(request):
   return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>')
 
 def about(request):
-  return HttpResponse('<h1>About</h1>') 
+  return render(request, 'about.html')
+
+def finches_index(request):
+    return render(request,'finches/index.html', {
+        'finches': finches
+    })
