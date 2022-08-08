59139f37db3b170ecffb5b2f82fc2afd15ea27ab
Dandd6541 <Mordkid4@gmail.com>
2022-08-08 12:39:42 -0700

    rendering some of the steps

--- main_app/views.py
+++ main_app/views.py
@@ -1,19 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-
-class Finch:  
-  def __init__(self, name, breed, description, age):
-    self.name = name
-    self.breed = breed
-    self.description = description
-    self.age = age
-
-finches = [
-  Finch('Lolo', 'tabby', 'foul little demon', 3),
-  Finch('Sachi', 'tortoise shell', 'diluted tortoise shell', 0),
-  Finch('Raven', 'black tripod', '3 legged finch', 4)
-]
+from main_app.models import Finch
 
 
 # Define the home view
@@ -23,7 +11,9 @@ def home(request):
 def about(request):
   return render(request, 'about.html')
 
+
 def finches_index(request):
+  finches = Finch.objects.all()
   return render(request,'finches/index.html', {
         'finches': finches
     })
