2065baa2a987306ebb67eb5638fc99e3151e2e2f
Dandd6541 <Mordkid4@gmail.com>
2022-08-11 18:51:37 -0700

    new update

--- main_app/views.py
+++ main_app/views.py
@@ -59,6 +59,10 @@ def assoc_toy(request, finch_id, toy_id):
   finch.toys.add(toy_id)
   return redirect('detail', finch_id=finch_id)
 
+def unassoc_toy(request, finch_id, toy_id):
+  Finch.objects.get(id=finch_id).toys.remove(toy_id)
+  return redirect('detail', finch_id=finch_id)
+
 class ToyList(ListView):
   model = Toy
 
